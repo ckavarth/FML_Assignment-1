cab37d228b2f347f9d92771657954d965d8501ec
ckavarth <158864561+ckavarth@users.noreply.github.com>
2024-02-04 17:00:14 -0800

    Add files via upload

--- FML_Assignment_1.R
+++ FML_Assignment_1.R
@@ -1,4 +1,4 @@
-## 1)Dataset:
+##1)Dataset:
 
 ## I have taken the dataset in web from this link below:
 ## https://www.kaggle.com/datasets/kukuroo3/body-signal-of-smoking?resource=download
@@ -9,7 +9,7 @@
 dataset = read.csv("C:\\Users\\Chaitu\\Desktop\\Body signal of smoking.csv", header = TRUE)
 dataset
 
-##Summary statistics for quantitative variables
+##3)Print out descriptive statistics for a selection of quantitative and categorical variables.
 #summary() function is used to calculate summary statistics for quantitative variables.
 quantitative_vars <- c("age", "height(cm)", "weight(kg)", "waist(cm)", "eyesight(left)", "eyesight(right)", "hearing(left)", "hearing(right)", "systolic", "relaxation", "fasting blood sugar", "Cholesterol", "triglyceride", "HDL", "LDL", "hemoglobin", "serum creatinine", "AST", "ALT", "Gtp")
 summary(quantitative_vars)
@@ -23,8 +23,9 @@ for (var in categorical_vars) {
   print(table(dataset[[var]]))
 }
 
-# Transform the 'weight(kg)' variable by taking the square root
-dataset$weight_sqrt <- sqrt(dataset$weight)
+##4)Transform the 'weight(kg)' variable by taking the square root
+sqrt_weight <- sqrt(dataset$weight)
+sqrt_weight
 
 #Print the first few rows of the dataset to see the transformed variable
 head(dataset)
@@ -32,6 +33,7 @@ head(dataset)
 # Filter data for individuals who smoke
 smoking_data <- dataset[dataset$smoking == "smoking", ]
 
+##5)Plot at least one quantitative variable, and one scatterplot
 # Plot scatterplot of Height(cm) vs. weight
 plot(dataset$height.cm., dataset$weight,
      xlab = "Height(cm)",
